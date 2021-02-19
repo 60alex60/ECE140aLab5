a952731e589fd8b839da08379f2b409c47421de3
60alex60 <36143720+60alex60@users.noreply.github.com>
2021-02-18 20:42:19 -0500

    Add files via upload

--- database/init-db.sql
+++ database/init-db.sql
@@ -13,3 +13,24 @@ DROP TABLE IF EXISTS Telemetry;
 
 -- CREATE TABLE Telemetry to store Telemetry data here
 -- Call it "Telemetry"!!!
+
+CREATE TABLE IF NOT EXISTS Telemetry (
+  id         int AUTO_INCREMENT PRIMARY KEY,
+  pitch      int,
+  roll       int,
+  yaw        int,
+  vgx        int,
+  vgy        int,
+  vgz        int,
+  templ      int,
+  temph      int,
+  tof        int,
+  h          int,
+  bat        int,
+  baro       double,
+  time       int,
+  agx        double,
+  agy        double,
+  agz        double, 
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
